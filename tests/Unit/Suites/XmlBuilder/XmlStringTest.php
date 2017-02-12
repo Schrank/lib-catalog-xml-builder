fbcaec080b307fb6f76811f360aebd8cb77983ea
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2017-02-12 22:40:32 +0100

    Issue #4: Name control characters explicitly, so people understand what we do

--- tests/Unit/Suites/XmlBuilder/XmlStringTest.php
+++ tests/Unit/Suites/XmlBuilder/XmlStringTest.php
@@ -10,6 +10,40 @@ use PHPUnit\Framework\TestCase;
  */
 class XmlStringTest extends TestCase
 {
+    const CONTROL_CHARACTERS = [
+        0x00 => 'Null',
+        0x01 => 'Start of Heading',
+        0x02 => 'Start of Text',
+        0x03 => 'End of Text',
+        0x04 => 'End of Transmission',
+        0x05 => 'Enquiry',
+        0x06 => 'Acknowledgement',
+        0x07 => 'Bell',
+        0x08 => 'Backspace[e][f]',
+        0x09 => 'Horizontal Tab[g]',
+        0x0B => 'Vertical Tab',
+        0x0C => 'Form Feed',
+        0x0E => 'Shift Out',
+        0x0F => 'Shift In',
+        0x10 => 'Data Link Escape',
+        0x11 => 'Device Control 1 (often XON)',
+        0x12 => 'Device Control 2',
+        0x13 => 'Device Control 3 (often XOFF)',
+        0x14 => 'Device Control 4',
+        0x15 => 'Negative Acknowledgement',
+        0x16 => 'Synchronous Idle',
+        0x17 => 'End of Transmission Block',
+        0x18 => 'Cancel',
+        0x19 => 'End of Medium',
+        0x1A => 'Substitute',
+        0x1B => 'Escape[j]',
+        0x1C => 'File Separator',
+        0x1D => 'Group Separator',
+        0x1E => 'Record Separator',
+        0x1F => 'Unit Separator',
+        0x7F => 'Delete[l][f]',
+    ];
+
     public function testReturnsSameDocument()
     {
         $xml = "<?xml version=\"1.0\"?>\n<xml/>";
@@ -20,7 +54,7 @@ class XmlStringTest extends TestCase
 
     public function testRemovesControlCharacters()
     {
-        $charactersToTest = array_merge(range(0x00, 0x09), [0x0B, 0x0C], range(0x0E, 0x1F), [0x7F]);
+        $charactersToTest = array_keys(self::CONTROL_CHARACTERS);
 
         $xml = '<?xml version="1.0"?><xml>';
         foreach ($charactersToTest as $c) {
